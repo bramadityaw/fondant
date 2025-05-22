ab577ebf19b96703e2737a999b90bff279f5024c
R. Bramaditya <bramadityaw@duck.com>
2025-05-22 18:50:18 +0700

    extended functions to take multiple arguments

--- language/src/lib.rs
+++ language/src/lib.rs
@@ -157,15 +157,43 @@ fn parse_let(input: &str) -> ParseResult<Expr> {
     ))
 }
 
+fn parse_args<'a>(input: &'a str) -> ParseResult<'a, Vec<String>> {
+    let mut args = Vec::new();
+    let (mut arg, mut rest) = parse_ident(input)?;
+    args.push(arg);
+    while rest.starts_with(",") {
+        (_, rest) = assume(",", rest)?;
+        (arg, rest) = parse_ident(rest)?;
+        if !arg.is_empty() {
+            args.push(arg);
+        }
+    }
+    let (_, rest) = optional(assume(",", rest), rest)?;
+    Ok((args, rest))
+}
+
+fn parse_params<'a>(input: &'a str) -> ParseResult<'a, Vec<Expr>> {
+    let mut params = Vec::new();
+    let (mut param, mut rest) = parse_expr(input)?;
+    params.push(param);
+    while rest.starts_with(",") {
+        (_, rest) = assume(",", rest)?;
+        (param, rest) = parse_expr(rest)?;
+        params.push(param);
+    }
+    let (_, rest) = optional(assume(",", rest), rest)?;
+    Ok((params, rest))
+}
+
 fn parse_fun<'a>(input: &'a str) -> ParseResult<'a, Expr> {
     let (_, rest) = assume("fun", input)?;
     let (_, rest) = assume("(", rest)?;
-    let (arg, rest) = parse_ident(rest)?;
+    let (args, rest) = parse_args(rest)?;
     let (_, rest) = assume(")", rest)?;
     let (body, rest) = either_one(vec![parse_digit, parse_var, parse_let].as_slice(), rest)?;
     Ok((
         Expr::Fun {
-            args: vec![arg],
+            args,
             body: Box::new(body),
         },
         rest,
@@ -175,12 +203,12 @@ fn parse_fun<'a>(input: &'a str) -> ParseResult<'a, Expr> {
 fn parse_app<'a>(input: &'a str) -> ParseResult<'a, Expr> {
     let (caller, rest) = either_one(vec![parse_paren_expr, parse_var].as_slice(), input)?;
     let (_, rest) = assume("(", rest)?;
-    let (param, rest) = parse_expr(rest)?;
+    let (params, rest) = parse_params(rest)?;
     let (_, rest) = assume(")", rest)?;
     Ok((
         Expr::App {
             caller: caller.into(),
-            params: vec![param],
+            params,
         },
         rest,
     ))
@@ -227,6 +255,12 @@ mod tests {
                 params: vec![Expr::Literal(4)],
             }
         });
+        assert_eq!(parse_app("x(4, 5)").unwrap().0, {
+            Expr::App {
+                caller: Expr::Var("x".into()).into(),
+                params: vec![Expr::Literal(4), Expr::Literal(5)],
+            }
+        });
         assert_eq!(parse_app("(fun(x) x)(4)").unwrap().0, {
             Expr::App {
                 caller: Expr::Fun {
@@ -244,6 +278,19 @@ mod tests {
         assert_eq!(parse_fun("fun(x) x").unwrap().0, {
             Expr::Fun {
                 args: vec!["x".into()],
+                body: Expr::Var("x".into()).into(),
+            }
+        });
+        assert_eq!(parse_fun("fun(x, y) x").unwrap().0, {
+            Expr::Fun {
+                args: vec!["x".into(), "y".into()],
+                body: Expr::Var("x".into()).into(),
+            }
+        });
+        // Trailing commas
+        assert_eq!(parse_fun("fun(x, y,) x").unwrap().0, {
+            Expr::Fun {
+                args: vec!["x".into(), "y".into()],
                 body: Expr::Var("x".into()).into(),
             }
         });
@@ -284,6 +331,7 @@ mod tests {
     #[test]
     fn eval_fun_app() {
         assert_eq!(eval("(fun(x) x)(4)"), "4");
+        assert_eq!(eval("(fun(x, y) x)(4, 5)"), "4");
     }
 
     #[test]
@@ -294,5 +342,6 @@ mod tests {
     #[test]
     fn let_fun_bindings() {
         assert_eq!(eval("let id = fun(x) x in id(4)"), "4");
+        assert_eq!(eval("let const = fun(x, y) x in const(4, 5)"), "4");
     }
 }
